84373c771d03d91e871d52c17837989ea7539cd1
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-03-24 06:50:46 -0300

    fix: padronize app and api user schemas

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 
 const authSchema = joi.object({
 	email: joi.string().email().max(160).required(),
-	password: joi.string().min(6).max(80).required(),
+	password: joi.string().min(5).max(80).required(),
 }).length(2)
 
 const tokenSchema = joi.object({
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 const userSchema = joi.object({
 	email: joi.string().email().max(160).required(),
 	username: joi.string().min(2).max(80).required(),
-	password: joi.string().min(6).max(80).required(),
+	password: joi.string().min(5).max(80).required(),
 	picture: joi.string().uri().required()
 }).length(4)
 
