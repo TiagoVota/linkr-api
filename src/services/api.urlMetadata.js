a7d560062fdcfd0b5434242e508a83268a0f6790
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-03-23 22:37:00 -0300

    feat: make function to get url-metada info

---
+++ src/services/api.urlMetadata.js
@@ -0,0 +1,29 @@
+import urlMetadata from 'url-metadata'
+
+
+const getUrl = async (url) => {
+	let urlInfo = {}
+	
+	await urlMetadata(url)
+		.then((data) => {
+			const image = data['twitter:image'] || data.image || data['og:image']
+
+			urlInfo = {
+				url: data.url,
+				title: data.title,
+				description: data.description,
+				image,
+			}
+		})
+		.catch(error => {
+			urlInfo = null
+			console.log('url-metadata error:\n', error)
+		})
+
+	return urlInfo
+}
+
+
+export {
+	getUrl,
+}
